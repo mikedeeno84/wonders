a30cf170aa9b66601af5be5fc9761c146a732ed2
Michael DeNatale <mikedeeno84@gmail.com>
2016-03-25 19:46:51 -0400

    insert client side socket implement event emitter override

--- server/io/index.js
+++ server/io/index.js
@@ -3,12 +3,18 @@ var socketio = require('socket.io');
 var io = null;
 
 module.exports = function (server) {
-
+		var numPlayers = 0;
     if (io) return io;
 
     io = socketio(server);
 
     io.on('connection', function () {
+    	numPlayers++;
+    	if(numPlayers>0){
+    		io.emit('join', {hi:'hi'});
+    	}
+    	console.log('socket')
+
         // Now have access to socket, wowzers!
     });
     
